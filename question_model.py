e1ebb788e5e6ff42cdb442a55d344d82c8412f9d
daniele.brant <daniele.brant@gmail.com>
2024-07-11 13:44:33 +0100

    "First Commit"

---
+++ question_model.py
@@ -0,0 +1,11 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+# my_question = Question("daniele", "yes")
+#
+# print(my_question.text)
+# print(my_question.answer)
